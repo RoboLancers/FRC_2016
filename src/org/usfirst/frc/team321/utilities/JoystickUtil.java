4017dddb3377982d70386183e7e760402f59b954
Henry Dang <henrydang1998@yahoo.com>
2016-01-15 18:59:09 -0500

    Added JoystickUtil to get axis values

---
+++ src/org/usfirst/frc/team321/utilities/JoystickUtil.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team321.utilities;
+
+import org.usfirst.frc.team321.robot.OI;
+
+public class JoystickUtil {
+
+	public final static double tolerance = 0.15;
+
+	public static double getLeftXAxisValue(){
+
+		if(OI.driveStick.getRawAxis(0) > Math.abs(tolerance)){
+			return OI.driveStick.getRawAxis(0);
+		}else{
+			return 0;
+		}
+	}
+
+	public static double getLeftYAxisValue(){
+
+		if(OI.driveStick.getRawAxis(0) > Math.abs(tolerance)){
+			return OI.driveStick.getRawAxis(1);
+		}else{
+			return 0;
+		}
+	}
+
+	public static double getRightXAxisValue(){
+
+		if(OI.driveStick.getRawAxis(0) > Math.abs(tolerance)){
+			return OI.driveStick.getRawAxis(1);
+		}else{
+			return 0;
+		}
+	}
+
+	public static double getRightYAxisValue(){
+
+		if(OI.driveStick.getRawAxis(0) > Math.abs(tolerance)){
+			return OI.driveStick.getRawAxis(1);
+		}else{
+			return 0;
+		}
+	}
+}
