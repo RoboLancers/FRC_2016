dbb2c384331d84d57f3ed4c2bf27b776d817a885
Henry Dang <henrydang1998@yahoo.com>
2016-01-29 15:58:58 -0500

    Robot now uses 6 speed controllers instead of 4

--- src/org/usfirst/frc/team321/robot/RobotMap.java
+++ src/org/usfirst/frc/team321/robot/RobotMap.java
@@ -7,9 +7,10 @@ package org.usfirst.frc.team321.robot;
  */
 public class RobotMap {
 	
-	public static final int FRONT_LEFT_MOTOR = 1;
-	public static final int FRONT_RIGHT_MOTOR = 2;
-	public static final int REAR_LEFT_MOTOR = 3;
-	public static final int REAR_RIGHT_MOTOR = 4;
-	
+	public static final int LEFT_FRONT_MOTOR = 1;
+	public static final int LEFT_MIDDLE_MOTOR = 2;
+	public static final int LEFT_BACK_MOTOR = 3;
+	public static final int RIGHT_FRONT_MOTOR = 4;
+	public static final int RIGHT_MIDDLE_MOTOR = 5;
+	public static final int RIGHT_BACK_MOTOR = 6;
 }
--- src/org/usfirst/frc/team321/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team321/robot/subsystems/DriveTrain.java
@@ -15,15 +15,18 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class DriveTrain extends Subsystem {
    
 	private static DriveTrain driveTrain;
-	private static SpeedController frontLeft, frontRight, rearLeft, rearRight;
+	private static SpeedController leftFront, leftBack, leftMiddle, rightFront, rightMiddle, rightBack;
 	
 	private DriveTrain(){
     	super("Drive Train");
     	
-    	frontLeft = new CANTalon(RobotMap.FRONT_LEFT_MOTOR);
-    	frontRight = new CANTalon(RobotMap.FRONT_RIGHT_MOTOR);
-    	rearLeft = new CANTalon(RobotMap.REAR_LEFT_MOTOR);
-    	rearRight = new CANTalon(RobotMap.REAR_RIGHT_MOTOR);
+    	leftFront = new CANTalon(RobotMap.LEFT_FRONT_MOTOR);
+    	leftMiddle = new CANTalon(RobotMap.LEFT_MIDDLE_MOTOR);
+    	leftBack = new CANTalon(RobotMap.RIGHT_FRONT_MOTOR);
+    	rightFront = new CANTalon(RobotMap.LEFT_BACK_MOTOR);
+    	rightMiddle = new CANTalon(RobotMap.RIGHT_MIDDLE_MOTOR);
+    	rightBack = new CANTalon(RobotMap.RIGHT_BACK_MOTOR);
+    	
     }
 	
 	public static DriveTrain getInstance(){
@@ -38,68 +41,29 @@ public class DriveTrain extends Subsystem {
         setDefaultCommand(new MoveWithJoystick());
     }
 
-	public SpeedController getFrontLeft() {
-		return frontLeft;
-	}
-
-	public void setFrontLeftPower(double power) {
-		if(Math.abs(power) <= 1){
-			DriveTrain.frontLeft.set(power);
-		}else{
-			throw new RuntimeException("frontLeft power is greater than 1 or less than -1");
-		}
-	}
-
-	public SpeedController getFrontRight() {
-		return frontRight;
-	}
-
-	public void setFrontRightPower(double power) {
-		if(Math.abs(power) <= 1){
-			DriveTrain.frontRight.set(power);
-		}else{
-			throw new RuntimeException("frontRight power is greater than 1 or less than -1");
-		}
-	}
-
-	public SpeedController getRearLeft() {
-		return rearLeft;
-	}
-
-	public void setRearLeftPower(double power) {
-		if(Math.abs(power) <= 1){
-			DriveTrain.rearLeft.set(power);
-		}else{
-			throw new RuntimeException("rearLeft power is greater than 1 or less than -1");
-		}
-	}
-
-	public SpeedController getRearRight() {
-		return rearRight;
-	}
-
-	public void setRearRightPower(double power) {
-		if(Math.abs(power) <= 1){
-			DriveTrain.rearLeft.set(power);
-		}else{
-			throw new RuntimeException("rearRight power is greater than 1 or less than -1");
-		}
-	}
-    
 	public void setLeftPowers(double power){
-		setFrontLeftPower(power);
-		setRearLeftPower(power);
+		if(Math.abs(power) <= 1){
+			leftFront.set(power);
+			leftMiddle.set(power);
+			leftBack.set(power);
+		}else{
+			throw new RuntimeException("Power was set greater than 1 or less than -1");
+		}
 	}
 	
 	public void setRightPowers(double power){
-		setFrontRightPower(power);
-		setRearRightPower(power);
+		if(Math.abs(power) <= 1){
+			rightFront.set(power);
+			rightMiddle.set(power);
+			rightBack.set(power);
+		}else{
+			throw new RuntimeException("Power was set greater than 1 or less than -1");
+		}
 	}
 	
 	public void setAllPowers(double power){
 		setLeftPowers(power);
 		setRightPowers(power);
 	}
-    
 }
 
