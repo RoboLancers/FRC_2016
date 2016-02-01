9d452dfd064ac594dc4f34e258bdf6bdc7add183
Henry Dang <henrydang1998@yahoo.com>
2016-02-01 16:37:44 -0500

    Change MoveWithJoystick to only have two possible drive types
    Either RCDrive or TankDrive

--- src/org/usfirst/frc/team321/robot/commands/MoveWithJoystick.java
+++ src/org/usfirst/frc/team321/robot/commands/MoveWithJoystick.java
@@ -1,8 +1,8 @@
 package org.usfirst.frc.team321.robot.commands;
 
-import org.usfirst.frc.team321.joystick_controller_types.JoystickControllerType;
-import org.usfirst.frc.team321.joystick_controller_types.TankDrive;
 import org.usfirst.frc.team321.robot.Robot;
+import org.usfirst.frc.team321.robot.subsystems.DriveTrain;
+import org.usfirst.frc.team321.utilities.JoystickUtil;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -11,7 +11,8 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class MoveWithJoystick extends Command {
 	
-	private JoystickControllerType joystickControllerType;
+	//private JoystickControllerType joystickControllerType;
+	boolean rcDrive = true;
 	
     public MoveWithJoystick() {
         requires(Robot.driveTrain);
@@ -19,12 +20,22 @@ public class MoveWithJoystick extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	setJoystickControllerType(new TankDrive());
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	joystickControllerType.drive();
+
+    	if(rcDrive){
+    		if(Math.abs(JoystickUtil.getLeftYAxisValue()) > 0){
+    			DriveTrain.setAllPowers(JoystickUtil.getLeftYAxisValue());
+    		}else{
+    			DriveTrain.setLeftPowers(JoystickUtil.getRightXAxisValue());
+    			DriveTrain.setRightPowers(JoystickUtil.getRightXAxisValue());
+    		}
+    	}else{
+    		DriveTrain.setLeftPowers(JoystickUtil.getLeftYAxisValue());
+	    	DriveTrain.setRightPowers(JoystickUtil.getRightYAxisValue());
+	    }
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -39,9 +50,5 @@ public class MoveWithJoystick extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    }
-    
-    protected void setJoystickControllerType(JoystickControllerType type){
-    	this.joystickControllerType = type;
     }
 }
