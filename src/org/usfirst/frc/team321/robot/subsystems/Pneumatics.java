ae8fc77dcff9c7638509c3b8013c8e40bdf3f15d
Henry Dang <henrydang1998@yahoo.com>
2016-02-06 14:22:03 -0500

    Changed Pneumatics subsystem since it does not use a compresor

--- src/org/usfirst/frc/team321/robot/subsystems/Pneumatics.java
+++ src/org/usfirst/frc/team321/robot/subsystems/Pneumatics.java
@@ -1,6 +1,6 @@
 package org.usfirst.frc.team321.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -8,37 +8,15 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Pneumatics extends Subsystem {
     
-	Compressor compressor;
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	public static DoubleSolenoid leftDoubleSolenoid;
+    public static DoubleSolenoid rightDoubleSolenoid;
 
+	public Pneumatics() {
+		leftDoubleSolenoid = new DoubleSolenoid(0, 1);
+		rightDoubleSolenoid = new DoubleSolenoid(2, 3);
+	}
+	
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
-    
-    public boolean isCompressorSafeToUse(){	
-    	if((compressor.getCompressorCurrentTooHighFault() && !compressor.getCompressorCurrentTooHighStickyFault()) ||
-   			(compressor.getCompressorNotConnectedFault() && !compressor.getCompressorNotConnectedStickyFault()) || 
-   			(compressor.getCompressorShortedFault() && !compressor.getCompressorShortedStickyFault())){
-    		return false;
-    	}else{
-    		return true;
-    	}
-    }
-    	
-    public double getPressure(){
-    	return compressor.getCompressorCurrent();
-    }
-    
-    public void regulateCompressor(){
-    	if(!compressor.getPressureSwitchValue() && !compressor.enabled()
-    			&& isCompressorSafeToUse()){
-    		compressor.start();
-    	}else if(compressor.getPressureSwitchValue() && compressor.enabled() 
-    			|| !isCompressorSafeToUse()){
-    		compressor.stop();
-    	}
-    }
+    }    
 }
 
