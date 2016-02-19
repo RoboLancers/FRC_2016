b4a8a4b92bd6496d866c2ddc2f1e40f1c4119462
Henry Dang <henrydang1998@yahoo.com>
2016-02-19 15:35:01 -0500

    Update DriveTrain to use regular Talons

--- src/org/usfirst/frc/team321/robot/RobotMap.java
+++ src/org/usfirst/frc/team321/robot/RobotMap.java
@@ -7,12 +7,12 @@ package org.usfirst.frc.team321.robot;
  */
 public class RobotMap {
 	
-	public static final int LEFT_FRONT_MOTOR = 3;
-	public static final int LEFT_MIDDLE_MOTOR = 4;
-	public static final int LEFT_BACK_MOTOR = 5;
-	public static final int RIGHT_FRONT_MOTOR = 0;
-	public static final int RIGHT_MIDDLE_MOTOR = 1;
-	public static final int RIGHT_BACK_MOTOR = 2;
+	public static final int LEFT_FRONT_MOTOR = 0;
+	public static final int LEFT_MIDDLE_MOTOR = 1;
+	public static final int LEFT_BACK_MOTOR = 2;
+	public static final int RIGHT_FRONT_MOTOR = 3;
+	public static final int RIGHT_MIDDLE_MOTOR = 4;
+	public static final int RIGHT_BACK_MOTOR = 5;
 	
 	public static final int ENC_L_A = 0;
 	public static final int ENC_L_B = 1;
--- src/org/usfirst/frc/team321/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team321/robot/subsystems/DriveTrain.java
@@ -5,9 +5,9 @@ import org.usfirst.frc.team321.robot.commands.MoveWithJoystick;
 import org.usfirst.frc.team321.utilities.LancerPID;
 import org.usfirst.frc.team321.utilities.MotorValueOutOfBoundsException;
 
-import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -21,21 +21,21 @@ public class DriveTrain extends Subsystem {
 	public static LancerPID PID_R,PID_L;
 	public static Encoder encoder_L,encoder_R; 
 	
-	private DriveTrain(){
+	public DriveTrain(){
     	super("Drive Train");
     	
-    	leftFront = new CANTalon(RobotMap.LEFT_FRONT_MOTOR);
-    	leftMiddle = new CANTalon(RobotMap.LEFT_MIDDLE_MOTOR);
-    	leftBack = new CANTalon(RobotMap.LEFT_BACK_MOTOR);
-    	rightFront = new CANTalon(RobotMap.RIGHT_FRONT_MOTOR);
-    	rightMiddle = new CANTalon(RobotMap.RIGHT_MIDDLE_MOTOR);
-    	rightBack = new CANTalon(RobotMap.RIGHT_BACK_MOTOR);
+    	leftFront = new Talon(RobotMap.LEFT_FRONT_MOTOR);
+    	leftMiddle = new Talon(RobotMap.LEFT_MIDDLE_MOTOR);
+    	leftBack = new Talon(RobotMap.LEFT_BACK_MOTOR);
+    	rightFront = new Talon(RobotMap.RIGHT_FRONT_MOTOR);
+    	rightMiddle = new Talon(RobotMap.RIGHT_MIDDLE_MOTOR);
+    	rightBack = new Talon(RobotMap.RIGHT_BACK_MOTOR);
     	
-    	PID_R = new LancerPID(1,1,.5,TICKS_PER_ROTATION/36);
-    	PID_L = new LancerPID(1,1,.5,TICKS_PER_ROTATION/36);
+    	//PID_R = new LancerPID(.001,.001,.0005,TICKS_PER_ROTATION/36);
+    	//PID_L = new LancerPID(.001,.001,.0005,TICKS_PER_ROTATION/36);
     	
-    	encoder_L = new Encoder(RobotMap.ENC_L_A,RobotMap.ENC_L_B);
-    	encoder_R = new Encoder(RobotMap.ENC_R_A,RobotMap.ENC_R_B);
+    	//encoder_L = new Encoder(RobotMap.ENC_L_A,RobotMap.ENC_L_B);
+    	//encoder_R = new Encoder(RobotMap.ENC_R_A,RobotMap.ENC_R_B);
     }
 
 	public static DriveTrain getInstance(){
@@ -52,12 +52,12 @@ public class DriveTrain extends Subsystem {
 	public static void setLeftPowers(double power){
 		if(Math.abs(power) <= 1){
 			
-			PID_L.setReference(power);
-			double left = PID_L.calcPID(encoder_L.getRate());
+			//PID_L.setReference(power);
+			//double left = PID_L.calcPID(encoder_L.getRate());
 			
-			leftFront.set(left);
-			leftMiddle.set(left);
-			leftBack.set(left);
+			leftFront.set(power);
+			leftMiddle.set(power);
+			leftBack.set(power);
 			
 		}else{
 			throw new MotorValueOutOfBoundsException();
@@ -67,12 +67,12 @@ public class DriveTrain extends Subsystem {
 	public static void setRightPowers(double power){
 		if(Math.abs(power) <= 1){
 			
-			PID_R.setReference(power);
-			double right = PID_R.calcPID(encoder_R.getRate());
+			//PID_R.setReference(power);
+			//double right = PID_R.calcPID(encoder_R.getRate());
 			
-			rightFront.set(right);
-			rightMiddle.set(right);
-			rightBack.set(right);
+			rightFront.set(power);
+			rightMiddle.set(power);
+			rightBack.set(power);
 			
 		}else{
 			throw new MotorValueOutOfBoundsException();
