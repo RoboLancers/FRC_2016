4b7f5a310d163914a7ff7924d87b6e732e8ea816
Henry Dang <henrydang1998@yahoo.com>
2016-02-24 13:40:58 -0500

    Refactor JoystickUtil

--- src/org/usfirst/frc/team321/utilities/JoystickUtil.java
+++ src/org/usfirst/frc/team321/utilities/JoystickUtil.java
@@ -7,14 +7,6 @@ public class JoystickUtil {
 
 	public final static double tolerance = 0.15;
 
-	public static double getLeftXAxisValue(){
-		if(Math.abs(OI.driveStick.getRawAxis(0)) > tolerance){
-			return OI.driveStick.getRawAxis(0);
-		}else{
-			return 0;
-		}
-	}
-
 	public static double getLeftYAxisValue(){
 		if(Math.abs(OI.driveStick.getRawAxis(1)) > tolerance){
 			return OI.driveStick.getRawAxis(1);
@@ -23,12 +15,8 @@ public class JoystickUtil {
 		}
 	}
 
-	public static double getRightXAxisValue(){
-		if(Math.abs(OI.driveStick.getRawAxis(4)) > tolerance){
-			return OI.driveStick.getRawAxis(4);
-		}else{
-			return 0;
-		}
+	public static double getLeftYAxisNormalized(){
+		return RobotUtil.squareAndKeepSign(getLeftYAxisValue());
 	}
 	
 	public static double getRightYAxisValue(){
@@ -39,16 +27,28 @@ public class JoystickUtil {
 		}
 	}
 	
-	public static double getLeftYAxisNormalized(){
-		return RobotUtil.squareAndKeepSign(getLeftYAxisValue());
-	}
-	
 	public static double getRightYAxisNormalized(){
 		return RobotUtil.squareAndKeepSign(getRightYAxisValue());
 	}
 	
+	public static double getLeftXAxisValue(){
+		if(Math.abs(OI.driveStick.getRawAxis(0)) > tolerance){
+			return OI.driveStick.getRawAxis(0);
+		}else{
+			return 0;
+		}
+	}
+	
 	public static double getLeftXAxisNormalized(){
 		return RobotUtil.squareAndKeepSign(getLeftXAxisValue());
+	}
+	
+	public static double getRightXAxisValue(){
+		if(Math.abs(OI.driveStick.getRawAxis(4)) > tolerance){
+			return OI.driveStick.getRawAxis(4);
+		}else{
+			return 0;
+		}
 	}
 	
 	public static double getRightXAxisNormalized(){
