e3212f2a86efa65478d50a9bca499725129b8a44
Henry Dang <henrydang1998@yahoo.com>
2016-02-24 12:59:41 -0500

    Update MoveIntakePivot to depend on IntakePivot subsystem

--- src/org/usfirst/frc/team321/robot/commands/MoveIntakePivot.java
+++ src/org/usfirst/frc/team321/robot/commands/MoveIntakePivot.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team321.robot.commands;
 import org.usfirst.frc.team321.robot.OI;
 import org.usfirst.frc.team321.robot.Robot;
 import org.usfirst.frc.team321.robot.subsystems.Intake;
+import org.usfirst.frc.team321.robot.subsystems.IntakePivot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -13,30 +14,30 @@ public class MoveIntakePivot extends Command {
 
 	boolean hasFinished = false;
 	double power;
-	Intake intake;
+	IntakePivot intakePivot;
 	
-    public MoveIntakePivot(double power) {
-        requires(Robot.intake);
-        this.intake = Robot.intake;
-        this.power = power;
+    public MoveIntakePivot() {
+        requires(Robot.intakePivot);
+        this.intakePivot = Robot.intakePivot;
     }
-
+    
     // Called just before this Command runs the first time
     protected void initialize() {
     	//unlock the motor first
-    	intake.unlockPivotMotor();
+    	hasFinished = false;
+    	intakePivot.unlockPivotMotor();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
-    	intake.setPivotMotor(power);
-    	
-    	if(!OI.maniBtn[2].get()){
-    		hasFinished = true;
-    	}else if(!OI.maniBtn[3].get()){
-    		hasFinished = true;
+    	if(OI.driveStick.getRawAxis(2)>.1){
+    		intakePivot.setPivotMotor(OI.driveStick.getRawAxis(2)/2);
     	}
+    	if(OI.driveStick.getRawAxis(3)>.1){
+    		intakePivot.setPivotMotor(-OI.driveStick.getRawAxis(3)/2);
+    	}
+    	
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -46,13 +47,15 @@ public class MoveIntakePivot extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	intake.lockPivotMotor();
+    	intakePivot.setPivotMotor(0);
+    	intakePivot.lockPivotMotor();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
     	hasFinished = true;
-    	intake.lockPivotMotor();
+    	intakePivot.setPivotMotor(0);
+    	intakePivot.lockPivotMotor();
     }
 }
