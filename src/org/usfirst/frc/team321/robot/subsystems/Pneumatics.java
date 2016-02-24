4f758c65b99a615736dd7444c5646c71ddca7a2f
Henry Dang <henrydang1998@yahoo.com>
2016-02-24 13:00:13 -0500

    Add compressor functionality into Pneumatics subsystem

--- src/org/usfirst/frc/team321/robot/subsystems/Pneumatics.java
+++ src/org/usfirst/frc/team321/robot/subsystems/Pneumatics.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team321.robot.subsystems;
 
+import org.usfirst.frc.team321.robot.RobotMap;
+import org.usfirst.frc.team321.robot.commands.RegulateCompressor;
+
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -11,13 +15,17 @@ public class Pneumatics extends Subsystem {
     
 	public DoubleSolenoid leftDoubleSolenoid;
     public DoubleSolenoid rightDoubleSolenoid;
-    
+    public Compressor compressor;
+
 	public Pneumatics() {
-		leftDoubleSolenoid = new DoubleSolenoid(0, 1);
-		rightDoubleSolenoid = new DoubleSolenoid(2, 3);
+		leftDoubleSolenoid = new DoubleSolenoid(4, 5);
+		rightDoubleSolenoid = new DoubleSolenoid(6, 7);
+		
+		compressor = new Compressor(RobotMap.COMPRESSOR);
 	}
 	
     public void initDefaultCommand() {
+    	setDefaultCommand(new RegulateCompressor());
     }    
     
     public Value getGear(){
@@ -40,5 +48,33 @@ public class Pneumatics extends Subsystem {
 	public void setRightDoubleSolenoid(DoubleSolenoid rightDoubleSolenoid) {
 		this.rightDoubleSolenoid = rightDoubleSolenoid;
 	}
+	public void regulateCompressor(){
+		/*
+		boolean isError = false;
+		
+		//Error Regulations
+		if((compressor.getCompressorCurrentTooHighFault() && !compressor.getCompressorCurrentTooHighStickyFault()) ||
+				(compressor.getCompressorNotConnectedFault() && !compressor.getCompressorNotConnectedStickyFault()) ||
+				(compressor.getCompressorShortedFault() && !compressor.getCompressorShortedStickyFault()))
+		{
+			//isError = true;
+		}else{
+			//isError = false; 
+		}
+
+		if(!compressor.getPressureSwitchValue() && !compressor.enabled() && !isError){
+			compressor.start();
+		}
+		else if((compressor.getPressureSwitchValue() && compressor.enabled()) || isError){
+			compressor.stop();
+		}*/
+		
+		compressor.start();
+	}
+	
+	public boolean getPressure(){
+		return compressor.getPressureSwitchValue();
+	}
+	
 }
 
